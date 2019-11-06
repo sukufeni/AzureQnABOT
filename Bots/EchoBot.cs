326dcce550780a51b55b48492456483394145824
Bruno Brandão <bruno.vieira@sonda.com>
2019-11-06 11:39:41 -0300

    Atualização de lista de nomes, correção de tamanho randomico

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -3,6 +3,7 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.5.0
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,6 +16,10 @@ namespace SlackBot.Bots
 {
     public class EchoBot : ActivityHandler
     {
+
+        private string[] names = { "João", "Felipe", "Bernardo", "Caio", "Jucemar", "Eduardo", "Lucas", "Paulo" };
+        private Random curr = new Random();
+
         public QnAMaker EchoBotQnA { get; private set; }
         public EchoBot(QnAMakerEndpoint endpoint)
         {
@@ -31,25 +36,22 @@ namespace SlackBot.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            foreach (var member in membersAdded)
-            {
-                if (member.Id != turnContext.Activity.Recipient.Id)
-                {
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Olá! :D"), cancellationToken);
-                }
-            }
+            int max = names.Length - 1;
+            await turnContext.SendActivityAsync(MessageFactory.Text($@"Seja bem vindo a SmartBot eu sou o {names[curr.Next(max)]}!"), cancellationToken);
         }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = new CancellationToken())
+        {
+            string messaeText = string.IsNullOrEmpty(((TurnContext)turnContext).Activity.Text) ? "Default" : ((TurnContext)turnContext).Activity.Text;
+        }
+
         private async Task AccessQnAMaker(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            turnContext.Activity.Text = turnContext.Activity.Text.ToLower();
+
             var results = await EchoBotQnA.GetAnswersAsync(turnContext);
-            if (results.Any())
-            {
-                await turnContext.SendActivityAsync(MessageFactory.Text(results.First().Answer), cancellationToken);
-            }
-            else
-            {
-                await turnContext.SendActivityAsync(MessageFactory.Text("Desculpe, Não consegui entender sua pergunta :/"), cancellationToken);
-            }
+            if (results.Any()) await turnContext.SendActivityAsync(MessageFactory.Text(results.First().Answer), cancellationToken);
+            else await turnContext.SendActivityAsync(MessageFactory.Text("Desculpe, Não consegui entender sua pergunta :/"), cancellationToken);
         }
     }
 }
